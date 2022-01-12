a716d3a8835a8c33477802975036a3dc3f1f0ad9
IR0KnotReally <71969213+IR0KnotReally@users.noreply.github.com>
2022-01-12 17:45:50 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,16 +6,7 @@ def swapFileData():
     with open("sample1.txt","w") as a:
         a.write(data_b)
     with open("sample2.txt","w") as b:
-        b.write(data_a)           
-    #filedata1=open("sample1.txt")
-    #filedata2=open("sample2.txt")
-    #print(filedata.read())
-    #lines=filedata1.readlines()
-    
-    #for i in lines:
-        #print(i)
-   # name="this is code.py"
-    #print(name.split())    
+        b.write(data_a)         
 
 swapFileData()
 
